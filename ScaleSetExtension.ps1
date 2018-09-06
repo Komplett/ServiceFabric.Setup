2bdb87895fbb3e7c36738d06008eb39cdb2c7e6e
Kenneth Dalgleish <kd@komplett.no>
2018-09-06 09:06:39 +0200

    Merge to one script and add parameters

--- ScaleSetExtension.ps1
+++ ScaleSetExtension.ps1
@@ -1,4 +1,10 @@
-﻿function Setup
+﻿[CmdletBinding()]
+Param (
+	$LoggEndPoint,
+	$NewRelicKey
+)
+
+function Setup
 {
 	PrepareDisks
     SetTimeZone
@@ -51,6 +57,34 @@ function PrepareDisks {
 		Format-Volume -FileSystem NTFS -NewFileSystemLabel $label.$count -Confirm:$false -Force
 		$count++
 	}
+}
+
+function Send-UdpDatagram
+{
+      Param ([string] $EndPoint, 
+      [int] $Port, 
+      [string] $Message)
+
+      $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+      $Address = [System.Net.IPAddress]::Parse($IP) 
+      $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port) 
+      $Socket = New-Object System.Net.Sockets.UDPClient 
+      $EncodedText = [system.Text.Encoding]::UTF8.GetBytes($Message) 
+      $SendMessage = $Socket.Send($EncodedText, $EncodedText.Length, $EndPoints) 
+      $Socket.Close() 
+} 
+
+function Log
+{
+    Param ([string] $Message, 
+    [string] $Level,
+    [string] $Logger) 
+
+    $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss,fff"
+    $HostName = $env:computername
+    $FormattedMessage = "$FormattedDate`r`napplication=Komplett.ServiceFabric.ScaleSet-Extension level=$Level logger=Komplett.ServiceFabric.ScaleSet-Extension.$Logger hostname=$HostName projectowner=Green`r`n$Message`r`n"
+    Write-Output $FormattedMessage
+    Send-UdpDatagram -EndPoint $LoggEndPoint -Port 666 -Message $FormattedMessage
 }
 
 Setup
