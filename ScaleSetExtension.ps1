6e111c8af239e671192149c285624db5e3bd7bad
Kenneth Dalgleish <kd@komplett.no>
2018-09-16 15:42:08 +0200

    Re-factor, order functions

--- ScaleSetExtension.ps1
+++ ScaleSetExtension.ps1
@@ -65,34 +65,6 @@ function PrepareDisks {
 	}
 }
 
-function Send-UdpDatagram
-{
-      Param ([string] $EndPoint, 
-      [int] $Port, 
-      [string] $Message)
-
-      $IP = [System.Net.Dns]::GetHostAddresses($EndPoint);
-      $Address = [System.Net.IPAddress]::Parse($IP);
-      $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port);
-      $Socket = New-Object System.Net.Sockets.UDPClient;
-      $EncodedText = [system.Text.Encoding]::UTF8.GetBytes($Message);
-      $SendMessage = $Socket.Send($EncodedText, $EncodedText.Length, $EndPoints);
-      $Socket.Close();
-} 
-
-function Log
-{
-    Param ([string] $Message, 
-    [string] $Level,
-    [string] $Logger) 
-
-    $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss,fff";
-    $HostName = $env:computername;
-    $FormattedMessage = "$FormattedDate`r`napplication=Komplett.ServiceFabric.ScaleSet-Extension level=$Level logger=Komplett.ServiceFabric.ScaleSet-Extension.$Logger hostname=$HostName projectowner=Green`r`n$Message`r`n";
-    Write-Output $FormattedMessage;
-    Send-UdpDatagram -EndPoint $LoggEndPoint -Port 666 -Message $FormattedMessage;
-}
-
 function SetupNewRelic
 {
     Param ([string] $Version,
@@ -154,6 +126,34 @@ function RunProcess
     } finally {
         $process.Dispose();
     };
+}
+
+function Send-UdpDatagram
+{
+      Param ([string] $EndPoint, 
+      [int] $Port, 
+      [string] $Message)
+
+      $IP = [System.Net.Dns]::GetHostAddresses($EndPoint);
+      $Address = [System.Net.IPAddress]::Parse($IP);
+      $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port);
+      $Socket = New-Object System.Net.Sockets.UDPClient;
+      $EncodedText = [system.Text.Encoding]::UTF8.GetBytes($Message);
+      $SendMessage = $Socket.Send($EncodedText, $EncodedText.Length, $EndPoints);
+      $Socket.Close();
+} 
+
+function Log
+{
+    Param ([string] $Message, 
+    [string] $Level,
+    [string] $Logger) 
+
+    $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss,fff";
+    $HostName = $env:computername;
+    $FormattedMessage = "$FormattedDate`r`napplication=Komplett.ServiceFabric.ScaleSet-Extension level=$Level logger=Komplett.ServiceFabric.ScaleSet-Extension.$Logger hostname=$HostName projectowner=Green`r`n$Message`r`n";
+    Write-Output $FormattedMessage;
+    Send-UdpDatagram -EndPoint $LoggEndPoint -Port 666 -Message $FormattedMessage;
 }
 
 Setup;
