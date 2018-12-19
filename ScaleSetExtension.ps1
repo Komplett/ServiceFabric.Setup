cb36ad1bcf1e4db025fe883e7579f8f4bdbdeda2
Kenneth Dalgleish <kd@komplett.no>
2018-12-19 16:00:53 +0100

    Re-write, stop using processinfo for installing chocolatey packages. Log only once pr execution

--- ScaleSetExtension.ps1
+++ ScaleSetExtension.ps1
@@ -10,54 +10,91 @@ Param (
    [Switch]$Clear
 )
 
+$global:errors = 0;
+$global:logFile = "log.txt";
+
 function Setup
 {
-    Log -Message "Setup starting" -Level "INFO" -Logger "Setup";
-
+	Remove-Item -path $global:logFile;
+	Start-Transcript -path $global:logFile;
+    Write-Event "Setup starting";
+	
     PrepareDisks;
     SetTimeZone;
     InstallCocolatey;
-    ChocoInstall -Package "dotnet4.7.2";
-    ChocoInstall -Package "dotnetcore-runtime";
-    ChocoInstall -Package "nodejs-lts";
+	RunChoco "choco install -y dotnet4.7.2";
+	RunChoco "choco install -y dotnetcore-runtime";
+	RunChoco "choco install -y nodejs-lts --version 10.14.2";
     SetupNewRelic -Version "8.6.45.0" -LicenseKey $NewRelicKey;
+	SetupNewRelicCore -Version "8.6.45.0" -LicenseKey $NewRelicKey;
+	
+	Write-Event "Setup Done";
 
-    Log -Message "Setup completed" -Level "INFO" -Logger "Setup";
+	Stop-Transcript
+	
+	LogResults;
+}
+
+function LogResults
+{
+	$level = "INFO";
+	if($global:errors -gt 0)
+	{
+		$level = "ERROR";
+	}
+	
+	$logData = Get-Content -Path $global:logFile -encoding UTF8 -Raw;
+	
+    Log -Message $logData -Level $level -Logger "ScaleSet-Extension";
+}
+
+function Write-Event
+{
+	Param ([string] $message)
+	Write-Output "";
+	Write-Output "------------------------------------------------------------------------";
+	Write-Output "$message";
+	Write-Output "------------------------------------------------------------------------";
+	Write-Output "";
+	
+}
+
+function RunChoco
+{
+	Param ([string] $Command)
+	
+	Write-Event "Running $Command";
+	$choco = "$env:ChocolateyInstall\bin\";
+	iex "$choco$Command";
+	if ($lastexitcode -ne 0) {
+		$global:errors++;
+	}
 }
 
 function SetTimeZone
 {
     $timeZone = "W. Europe Standard Time";
     $cmdOutPut = Set-TimeZone $timeZone *>&1 | Out-String;
-    Log -Message "Setting timezone to $timeZone`r`n$cmdOutput" -Level "INFO" -Logger "SetTimeZone";
+    Write-Event "Setting timezone to $timeZone`r`n$cmdOutput";
 }
 
 function InstallCocolatey
 {
+	Write-Event "Install chocolatey";
     choco.exe -v;
     $Output = $?;
     If (-Not $Output) {  
         $cmdOutput = iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) *>&1 | Out-String;
-        Log -Message "Installing Chocolatey`r`n$cmdOutput" -Level "WARN" -Logger "ChocolateySetup";
+        Write-Output "Installing Chocolatey`r`n$cmdOutput";
     }
     else {
-        Log -Message "Chocolatey already installed, skipping." -Level "INFO" -Logger "ChocolateySetup";
+        Write-Output "Chocolatey already installed, skipping.";
     }
-}
-
-function ChocoInstall
-{
-    Param ([string] $Package,
-    [string] $Version = "",
-    [string] $Params = "",
-    [string] $Flags = "")  
-
-    $Command = "$env:ChocolateyInstall\bin\choco.exe";
-    $Arguments = "install -y $Flags $Package --version=`"$Version`" --params=`"$Params`"";
-    RunProcess -Command $Command -Arguments $Arguments -Logger "ChocoInstall" -Description "Installing $Package from Chocolatey";    
 }
 
 function PrepareDisks {
+	Write-Event "Preparing disk if needed.";
+	
 	$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number;
 
 	$letters = 70..89 | ForEach-Object { [char]$_ };
@@ -78,38 +115,88 @@ function SetupNewRelic
 {
     Param ([string] $Version,
     [string] $LicenseKey)
-    
+	
+	Write-Event "Installing New Relic Agent";
+	
+	$newRelicAgent = "$env:Programfiles\New Relic\.Net Agent\NewRelic.Agent.Core.dll";
+	
+	if (![System.IO.File]::Exists($newRelicAgent)) {
+		Write-Output "New Relic Agent missing, installing.";
+		InstallNewRelic -Version $Version -LicenseKey $LicenseKey;
+	}
+	else {
+        Write-Output "New Relic Agent already installed, skipping.";
+    }
+}
+
+function SetupNewRelicCore
+{
+    Param ([string] $Version,
+    [string] $LicenseKey)
+	
+	Write-Event "Installing New Relic Core Agent";
+	
+	$newRelicCoreAgent = "$env:Programfiles\New Relic\.NetCore Agent\NewRelic.Agent.Core.dll";
+	
+	if (![System.IO.File]::Exists($newRelicCoreAgent)) {
+		Write-Output "New Relic Core Agent missing, installing.";
+		InstallNewRelicCore -Version $Version -LicenseKey $LicenseKey;
+	}
+	else {
+        Write-Output "New Relic Core Agent already installed, skipping.";
+    }
+}
+
+function InstallNewRelic
+{
+    Param ([string] $Version,
+    [string] $LicenseKey)	
+	
     $CurrentFolder = $PSScriptRoot;
     $DotNetUrl = "https://download.newrelic.com/dot_net_agent/previous_releases/$Version/newrelic-agent-win-$Version-scriptable-installer.zip";
-    $CoreUrl = "https://download.newrelic.com/dot_net_agent/previous_releases/$Version/newrelic-netcore20-agent-win-$Version-scriptable-installer.zip";
     $DotNetFile = "$PSScriptRoot\NewRelicDotNet-$Version.zip";
-    $CoreNetFile = "$PSScriptRoot\NewReliCore-$Version.zip";
     $DotNetFolder = "$PSScriptRoot\NewRelicDotNet";
-    $CoreFolder = "$PSScriptRoot\NewRelicCore";
 
     ResetNewRelicEnvironmentVariables;
 
     (New-Object System.Net.WebClient).DownloadFile($DotNetUrl, $DotNetFile);
-    (New-Object System.Net.WebClient).DownloadFile($CoreUrl, $CoreNetFile);
 
     Expand-Archive -Force $DotNetFile -DestinationPath $DotNetFolder;
-    Expand-Archive -Force $CoreNetFile -DestinationPath $CoreFolder;
 
     Set-Location -Path $DotNetFolder -PassThru;
     $OutPut = & "$DotNetFolder\install.cmd" -LicenseKey $LicenseKey -NoIISReset -InstrumentAll -ForceLicenseKey *>&1 | Out-String;;
-    # RunProcess -Command "$DotNetFolder\install.cmd" -Arguments "-LicenseKey $LicenseKey -NoIISReset -InstrumentAll -ForceLicenseKey" -Logger "NewRelicDotNetInstall" -Description "Installing NewRelic .Net Agent $Version";    
-    Log -Message "Installed NewRelic .Net Agent $Version`n$OutPut" -Level "INFO" -Logger "NewRelicDotNetInstall";
+    Write-Output "Installed NewRelic .Net Agent $Version`n$OutPut";
+
+    Set-Location -Path $CurrentFolder -PassThru;
+    # Cleanup
+    Remove-Item $DotNetFile;
+    Remove-Item –path $DotNetFolder –recurse
+}
+
+function InstallNewRelicCore
+{
+    Param ([string] $Version,
+    [string] $LicenseKey)	
+	
+    $CurrentFolder = $PSScriptRoot;
+    $CoreUrl = "https://download.newrelic.com/dot_net_agent/previous_releases/$Version/newrelic-netcore20-agent-win-$Version-scriptable-installer.zip";
+    $CoreNetFile = "$PSScriptRoot\NewReliCore-$Version.zip";
+    $CoreFolder = "$PSScriptRoot\NewRelicCore";
+
+    ResetNewRelicCoreEnvironmentVariables;
+
+    (New-Object System.Net.WebClient).DownloadFile($CoreUrl, $CoreNetFile);
+
+    Expand-Archive -Force $CoreNetFile -DestinationPath $CoreFolder;
 
     Set-Location -Path $CoreFolder -PassThru
     $OutPut = & "$CoreFolder\installAgent.ps1" -destination "$Env:Programfiles\New Relic\.NetCore Agent" -installType global -licenseKey $LicenseKey -Force *>&1 | Out-String;
-    Log -Message "Installed NewRelic .Net Core Agent $Version`n$OutPut" -Level "INFO" -Logger "NewRelicDotNetCoreInstall";
+    Write-Output "Installed NewRelic .Net Core Agent $Version`n$OutPut";
 
     Set-Location -Path $CurrentFolder -PassThru;
 
     # Cleanup
-    Remove-Item $DotNetFile;
     Remove-Item $CoreNetFile;
-    Remove-Item –path $DotNetFolder –recurse
     Remove-Item –path $CoreFolder –recurse
 }
 
@@ -121,42 +208,15 @@ function ResetNewRelicEnvironmentVariables
     [Environment]::SetEnvironmentVariable("COR_ENABLE_PROFILING", "1", "Machine");
     [Environment]::SetEnvironmentVariable("COR_PROFILER", "{71DA0A04-7777-4EC6-9643-7D28B46A8A41}", "Machine");
     [Environment]::SetEnvironmentVariable("COR_PROFILER_PATH", "", "Machine");
-    [Environment]::SetEnvironmentVariable("NEWRELIC_HOME", "", "Machine");
+    [Environment]::SetEnvironmentVariable("NEWRELIC_HOME", "", "Machine");    
+}
+
+function ResetNewRelicCoreEnvironmentVariables
+{
     [Environment]::SetEnvironmentVariable("CORECLR_ENABLE_PROFILING", "", "Machine");
     [Environment]::SetEnvironmentVariable("CORECLR_PROFILER", "", "Machine");
     [Environment]::SetEnvironmentVariable("CORECLR_PROFILER_PATH", "", "Machine");
     [Environment]::SetEnvironmentVariable("CORECLR_NEWRELIC_HOME", "", "Machine");
-}
-
-function RunProcess
-{
-    Param ([string] $Command,
-    [string] $Arguments,
-    [string] $Logger,
-    [string] $Description)  
-    
-    $process = [System.Diagnostics.Process]::new();
-
-     try {
-        $process.StartInfo = [System.Diagnostics.ProcessStartInfo]::new($Command, $Arguments);
-        $process.StartInfo.RedirectStandardOutput = $true;
-        $process.StartInfo.RedirectStandardError = $true;
-        $process.StartInfo.UseShellExecute = $false;
-        $process.StartInfo.CreateNoWindow = $false;
-		$process.StartInfo.Verb = "runas";
-        $null = $process.Start();
-        $process.WaitForExit(1000 * 60 * 3);
-        $stdOut = $process.StandardOutput.ReadToEnd();
-        $stdErr = $process.StandardError.ReadToEnd();
-
-        if ( $process.ExitCode -eq 0 ) {            
-            Log -Message "Success: $Description`r`n$stdOut" -Level "INFO" -Logger $Logger
-        } else {
-            Log -Message "Error: $Descriptionr`n$stdErr`r`n$stdOut" -Level "ERROR" -Logger $Logger
-        };
-    } finally {
-        $process.Dispose();
-    };
 }
 
 function Send-UdpDatagram
@@ -182,8 +242,8 @@ function Log
 
     $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss,fff";
     $HostName = $env:computername;
-    $FormattedMessage = "$FormattedDate`r`napplication=Komplett.ServiceFabric.ScaleSet-Extension level=$Level logger=Komplett.ServiceFabric.ScaleSet-Extension.$Logger hostname=$HostName projectowner=Green`r`n$Message`r`n";
-    Write-Output $FormattedMessage;
+	$messageBase = "$FormattedDate`r`napplication=Komplett.ServiceFabric.ScaleSet-Extension level=$Level logger=Komplett.ServiceFabric.ScaleSet-Extension.$Logger hostname=$HostName projectowner=Green`r`n";
+    $FormattedMessage = $messageBase + $Message;
     Send-UdpDatagram -EndPoint $LoggEndPoint -Port 666 -Message $FormattedMessage;
 }
 
